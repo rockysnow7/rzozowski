38a6ac09a3be9f7a9a920ce5f6a2ee7174edaa97
Finn Eyles <feyles@icloud.com>
2025-04-14 18:35:51 +0100

    one (1) integration test

---
+++ tests/tests.rs
@@ -0,0 +1,12 @@
+use rzozowski::{parse, Regex};
+
+#[test]
+fn test_parse_and_matches() {
+    let regex = parse("(a|b)*c+").unwrap();
+    assert!(regex.matches("c"));
+    assert!(regex.matches("cc"));
+    assert!(regex.matches("ac"));
+    assert!(regex.matches("abc"));
+    assert!(regex.matches("abbaccc"));
+    
+}

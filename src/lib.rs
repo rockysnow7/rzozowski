2cbdd881dc7f9f554cd71d76afd2e2d5706e1205
Finn Eyles <feyles@icloud.com>
2025-04-14 22:17:42 +0100

    added doc comment to crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![warn(unused_crate_dependencies)]
 
+//! *rzozowski* (ruh-zov-ski) is a Rust crate for reasoning about regular expressions in terms of Brzozowski derivatives.
+
 mod derivatives;
 mod parser;
 
